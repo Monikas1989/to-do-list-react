c4d2c4d432bacf6864688564549de962ff6ddf8b
Monika Kazula <lorika09022014@gmail.com>
2022-03-26 15:54:14 +0100

    Updates

---
+++ precache-manifest.50567e350740a17fb4e38c918990ab46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "61671b8218c87efea602",
+    "url": "/static/css/main.7d4408fa.chunk.css"
+  },
+  {
+    "revision": "61671b8218c87efea602",
+    "url": "/static/js/main.61671b82.chunk.js"
+  },
+  {
+    "revision": "97af9aaba510c3e4c76e",
+    "url": "/static/js/1.97af9aab.chunk.js"
+  },
+  {
+    "revision": "45de306f3f336216fdf0",
+    "url": "/static/js/2.45de306f.chunk.js"
+  },
+  {
+    "revision": "7ee0e2da555c3fd35366",
+    "url": "/static/js/runtime~main.7ee0e2da.js"
+  },
+  {
+    "revision": "d359d59f352af0fbab46461774e23168",
+    "url": "/index.html"
+  }
+];
